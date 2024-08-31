90353e2b5dc836356cb977153e9cd9ffcb4ce6d1
NomanAhmed <officialnoman515@gmail.com>
2024-08-31 07:49:38 +0300

    Update Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import DashboardLayout from "../../components/DashboardLayout";
 import Currentprice from "./components/Currentprice";
 import PortfolioSection from "./components/PortfolioSection";
+import Transactions from "./components/Transactions";
 
 const Dashboard = () => {
   return (
@@ -19,6 +20,9 @@ const Dashboard = () => {
           </GridItem>
           <GridItem colSpan={1}>
             <Currentprice />
+          </GridItem>
+          <GridItem colSpan={1}>
+            <Transactions />
           </GridItem>
         </Grid>
       </DashboardLayout>
---
+++ src/pages/Dashboard/components/Transactions.jsx
@@ -0,0 +1,69 @@
+import { Box, Divider, Flex, Grid, Icon, Stack, Text } from "@chakra-ui/react";
+import { CustumCard } from "../../../Chakra/Customcart";
+import { TbCurrencyBahraini } from "react-icons/tb";
+import { PiCurrencyBtcFill } from "react-icons/pi";
+import { Fragment } from "react";
+
+const Transactions = () => {
+  const transaction = [
+    {
+      id: "1",
+      icon: TbCurrencyBahraini,
+      text: "BHD Deposite",
+      amount: "+ BHD81,123.10",
+      timestamp: "2024-30-08 09:10 PM",
+    },
+    {
+      id: "2",
+      icon: PiCurrencyBtcFill,
+      text: "BTC Sell",
+      amount: "- 12.48513391 BTC",
+      timestamp: "2024-26-08 03:10 AM",
+    },
+    {
+      id: "3",
+      icon: TbCurrencyBahraini,
+      text: "BHD Deposite",
+      amount: "+ BHD81,123.10",
+      timestamp: "2024-30-08 09:10 PM",
+    },
+  ];
+  return (
+    <CustumCard>
+      <Text mb="6" fontSize="sm" color="black.80">
+        Recent Transaction
+      </Text>
+      <Stack spacing={4}>
+        {transaction.map((recentTransfer, i) => {
+          return (
+            <Fragment key={recentTransfer.id}>
+              {i == !0 && <Divider />}
+              <Flex gap="4">
+                <Grid
+                  placeItems="center"
+                  bg="black.5"
+                  textAlign="center"
+                  boxSize={10}
+                  borderRadius="full"
+                >
+                  <Icon as={recentTransfer.icon} />
+                </Grid>
+                <Flex justify="space-between" w="full">
+                  <Stack spacing={0}>
+                    <Text textStyle="h6">{recentTransfer.text}</Text>
+                    <Text fontSize="small" color="black.40">
+                      {recentTransfer.timestamp}
+                    </Text>
+                  </Stack>
+                  <Text textStyle="h6">{recentTransfer.amount}</Text>
+                </Flex>
+              </Flex>
+            </Fragment>
+          );
+        })}
+      </Stack>
+    </CustumCard>
+  );
+};
+
+export default Transactions;
